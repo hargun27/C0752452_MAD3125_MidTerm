3fe3eb72b6df6ecc189d9f80254900be7716161d
hargunmarya <hargunmarya2726@gmail.com>
2019-08-08 20:00:13 -0400

    [Hargun] login activity added

---
+++ app/src/main/java/com/hargun/c0752452_mad3125_midterm/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.hargun.c0752452_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
