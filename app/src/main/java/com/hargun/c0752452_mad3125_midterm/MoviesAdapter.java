7aafaf3c19e20089d87fe948a0cdf7251f72b3de
hargunmarya <hargunmarya2726@gmail.com>
2019-08-09 17:43:20 -0400

    [Hargun] recycler view added

---
+++ app/src/main/java/com/hargun/c0752452_mad3125_midterm/MoviesAdapter.java
@@ -0,0 +1,4 @@
+package com.hargun.c0752452_mad3125_midterm;
+
+class MoviesAdapter {
+}
---
+++ app/src/main/java/com/hargun/c0752452_mad3125_midterm/Row.java
@@ -0,0 +1,50 @@
+package com.hargun.c0752452_mad3125_midterm;
+
+public class Row {
+    String flightimage;
+    String flightName;
+    String flightYear;
+
+    public Row() {
+    }
+
+    public Row(String image, String flightName, String flightYear) {
+        this.flightimage = image;
+        this.flightName = flightName;
+        this.flightYear = flightYear;
+    }
+
+    public String getFlightImage() {
+        return flightimage;
+    }
+
+    public void setFlightImage(String image) {
+        this.flightimage = image;
+    }
+
+    public String getFlightName() {
+        return flightName;
+    }
+
+    public void setFlightName(String flightName) {
+        this.flightName = flightName;
+    }
+
+    public String getFlightYear() {
+        return flightYear;
+    }
+
+    public void setFlightYear(String flightYear) {
+        this.flightYear = flightYear;
+    }
+    @Override
+    public String toString() {
+        return "Row{" +
+                "image='" + flightimage + '\'' +
+                ", flightName='" + flightName + '\'' +
+                ", flightYear='" + flightYear + '\'' +
+                '}';
+    }
+}
+
+
